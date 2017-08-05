f20e9141137b54d9b208518d05a2d7976199b501
Alan McLarney <alanmclarney@gmail.com>
2017-08-05 19:39:44 +0100

    guitar string test added & passed

--- music_manager_specs/GuitarTest.java
+++ music_manager_specs/GuitarTest.java
@@ -1,3 +1,19 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 import music_manager.*;
+
+public class GuitarTest {
+
+  Guitar guitar;
+
+  @Before
+  public void before(){
+    guitar = new Guitar("Strum!", 6);
+  }
+
+  @Test
+  public void hasStrings(){
+    assertEquals(6, guitar.numberOfStrings());
+  }
+
+}
